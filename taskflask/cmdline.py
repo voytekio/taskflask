4136329c66795539a480149604ff60263e3c146d
voytekio <voytek@voytek.io>
2019-11-23 13:04:23 +0000

    improve tag/heading handling

--- taskflask/cmdline.py
+++ taskflask/cmdline.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import pdb
 
 import taskflask.tklr as tklrlib
@@ -7,15 +8,22 @@ def run_flask():
     api.app.run(debug=True)
 
 if __name__ == '__main__':
-    api.app.run(host= '0.0.0.0', debug=True)
-    sys.exit(1)
+    #api.app.run(host= '0.0.0.0', debug=True)
+    #sys.exit(1)
     #pdb.set_trace()
     tklr = tklrlib.Tklr('/installers/tklr_0_2.txt')
     #tklr.find_major_sections()
-    tklr.find_sections(tklr.sections, tag='========== ', ignore='e.of', regex=r'========== ')
-    tklr.find_sections(tklr.subsections, tag='    ', ignore='==', regex=r'    \S')
-    tklr.load_dict()
-    tklr.print_section('M5')
+    tklr.load_full_dict()
+    #tklr.find_sections(tklr.sections, tag='========== ', ignore='e.of', regex=r'========== ')
+    #tklr.find_sections(tklr.subsections, tag='    ', ignore='==', regex=r'    \S')
+    #tklr.load_dict()
+    #print(tklr.get_section('M5'),end="")
+    #print(tklr.get_section('M6'),end="")
+    pdb.set_trace()
+    tklr.move_section('09', '10')
+    #print(tklr.get_section('M5'),end="")
+    #print(tklr.get_section('M6'),end="")
+    tklr.print_all()
     '''
     def find_major_sections(self):
         self.find_sections(self, self.sections, tag='========== ', ignore='e.of')
--- taskflask/tklr.py
+++ taskflask/tklr.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from collections import OrderedDict
 import six
 import re
@@ -21,12 +22,26 @@ class Tklr():
             'BRAINDEAD',
             'DONEs',
         ]
+        self.searchtags = {
+            'INs': {
+                'tag': '    ',
+                'ignore': '==',
+                'regex': r'    \S'},
+            'projects': {
+                'tag': '',
+                'ignore': '=====',
+                'regex': r'\S'},
+            'calendar': {
+                'tag': '    ',
+                'ignore': '==',
+                'regex': r'    \S'},
+        }
 
     def match_keyword(self, line):
         for keyword in self.keywords:
-            print('kword: {}, line: {}'.format(keyword, line))
+            #print('kword: {}, line: {}'.format(keyword, line))
             if keyword in line:
-                print('SKIP')
+                #print('SKIP')
                 return True
         return False
 
@@ -35,52 +50,110 @@ class Tklr():
             for one_line in f:
                 self.file_contents.append(one_line.strip('\n'))
 
+    def load_full_dict(self):
+        #pdb.set_trace()
+        # compute major sections metadata
+        self.find_sections(self.sections, tag='========== ', ignore='e.of', regex=r'========== ')
+        # compute minor subsections metadata
+        for section in self.sections:
+            name = section[0]
+            self.find_sections(self.subsections, tag=self.searchtags[name]['tag'], ignore=self.searchtags[name]['ignore'], regex=self.searchtags[name]['regex'], start=section[1], end=section[2])
+        # store actual contents in dict
+        for one_subsection in self.subsections:
+            self.dict[one_subsection[0]] = {}
+            self.dict[one_subsection[0]]['heading'] = one_subsection[3]
+            self.dict[one_subsection[0]]['contents'] = self.file_contents[one_subsection[1]:one_subsection[2]]
+
+
+    """
     def load_dict(self):
+        '''
+        go through all subsection markers and read file contents and put into a dict structure
+        '''
         #pdb.set_trace()
         for one_subsection in self.subsections:
             self.dict[one_subsection[0]] = self.file_contents[one_subsection[1]:one_subsection[2]]
+    """
 
-    def find_sections(self, list_to_update, tag='========== ', ignore='e.of', regex=r'    \S'):
+
+    def find_sections(self, list_to_update, tag='========== ', ignore='e.of', regex=r'    \S', start=0, end=None):
+        '''
+        using magic tags, discover subsections and note their names, start and end locations
+        this only stores metadata about subsections into a list, load_dict func will 
+        use this to retrieve data and store actual contents. 
+        '''
+        #pdb.set_trace()
         if not self.file_contents:
             self.read_file(self.filename)
-        for line_counter, one_line in enumerate(self.file_contents):
+        temp_list = []
+        temp_list = self.file_contents[start:] if not end else self.file_contents[start:end]
+        #for line_counter, one_line in enumerate(self.file_contents):
+        for line_counter, one_line in enumerate(temp_list):
             if re.match(regex, one_line) and ignore not in one_line and not self.match_keyword(one_line):
                 #pdb.set_trace()
                 tag_name = one_line.strip(tag).rstrip('\n')
                 tag_name = tag_name.split(':')[0]
-                tag_start = line_counter+1
-                list_to_update.append((tag_name, tag_start, 0))
+                section_full_name = one_line
+                section_start = line_counter+1 + start
+                list_to_update.append((tag_name, section_start, 0, section_full_name))
         # easiest way to calculate section end is to look at start of next element
         #pdb.set_trace()
         for count, one_section in enumerate(list_to_update):
             try:
-                tag_end = list_to_update[count+1][1] - 1
+                section_end = list_to_update[count+1][1] - 1 # + start
             except IndexError:
-                list_to_update[count] = (list_to_update[count][0], list_to_update[count][1], line_counter+1)
+                #pdb.set_trace()
+                list_to_update[count] = (list_to_update[count][0], list_to_update[count][1], line_counter+1 + start, list_to_update[count][3])
             else:
-                list_to_update[count] = (list_to_update[count][0], list_to_update[count][1], tag_end)
+                list_to_update[count] = (list_to_update[count][0], list_to_update[count][1], section_end, list_to_update[count][3])
+
+    def move_tdy(self):
+        #day = figure_day():
+        #move_section(day, day+1)
+        return 0
 
     def move_section(self, section_from, section_to):
-        buffer_space = self.dict[section_to] + self.dict[section_from]
-        self.dict[section_to] = buffer_space
-        self.dict[section_from] = ''
+        '''
+        takes contents of one section and appends/prepends to another
+        does not move headings, only contents
+        '''
+        buffer_space = self.dict[section_to]['contents'] + self.dict[section_from]['contents']
+        self.dict[section_to]['contents'] = buffer_space
+        self.dict[section_from]['contents'] = ''
+
+    def get_section(self, section_name):
+        ret = ''
+        #pdb.set_trace()
+        ret = self.dict[section_name]['heading'] + '\n'
+        for line in self.dict[section_name]['contents']:
+            ret += line + '\n'
+        return ret
 
     def print_section(self, section_name):
-        for line in self.dict[section_name]:
-            print(line)
+        return 'dont use, use get_section instead'
+        ret = ''
+        for line in self.dict[section_name]['contents']:
+            ret += line + '\n'
+        return ret
 
-    def print_whole_thing(self):
-        for k, v in six.iteritems(self.dict):
-            print(k)
-            self.print_section(k)
+    def print_all(self):
+        new_filename = '{}-1'.format(self.filename)
+        with open(new_filename, 'w') as f:
+            for k, v in six.iteritems(self.dict): #print(k)
+                #pdb.set_trace()
+                #print(v['heading'])
+                #f.write(v['heading']+'\n')
+                print(self.get_section(k), end="")
+                f.write(self.get_section(k))
 
     def __str__(self):
+        print('!!! old - may need revisiting')
         print_string = ''
         for one_section in self.sections:
             print_string += ('Section {}, start: {}, end: {}\n'.format(one_section[0], one_section[1], one_section[2]))
         for one_subsection in self.subsections:
             print_string += ('Section {}, start: {}, end: {}\n'.format(one_subsection[0], one_subsection[1], one_subsection[2]))
-        pdb.set_trace()
+        #pdb.set_trace()
         return print_string
 
 
