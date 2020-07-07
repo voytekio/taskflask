ba6e608f4588f34f345129d0d5d2e20394cf96de
voytekio <voytek@voytek.io>
2020-07-07 01:51:04 +0000

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='taskflaskapi',
-        version = '0.1.6',
+        version = '0.1.7',
         description = 'tasks app with api in flask',
         author = 'Voytek Krudysz',
         author_email = 'voytek@voytek.io',
