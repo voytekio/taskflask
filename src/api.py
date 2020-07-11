21840ac4e82627711de500e28311063e6113601e
voytekio <voytek@voytek.io>
2020-07-11 02:44:24 +0000

    lint api and tklr

--- src/api.py
+++ src/api.py
@@ -1,15 +1,18 @@
 #!flask/bin/python
-import random
+'''
+simple flask file to get started
+'''
+from __future__ import absolute_import, print_function
 from subprocess import Popen, PIPE
 import shlex
+import pdb  # pylint: disable=unused-import
 
 from flask import Flask, jsonify, abort, make_response, request
 
-import pdb
 
-app = Flask(__name__)
+app = Flask(__name__)  # pylint: disable=invalid-name
 
-tasks = [
+tasks = [  # pylint: disable=invalid-name
     {
         'id': 1,
         'title': u'Buy groceries',
@@ -24,46 +27,46 @@ tasks = [
     }
 ]
 @app.errorhandler(404)
-def not_found(error):
+def not_found(error):  # pylint: disable=unused-argument
+    ''' flask error handler for 404s '''
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-def run_cmd(cmd_name, debug=False):
-    print('running cmd: {}'.format(cmd_name))
+def run_cmd(cmd_name):
+    ''' cmd runner that was written too quick '''
+    print('USING OLD RUN_CMD, NEED TO REFACTOR THIS')  # pylint: disable=print-statement
+    print('running cmd: {}'.format(cmd_name))  # pylint: disable=print-statement
     cmdplus = shlex.split(cmd_name)
     process = Popen(cmdplus, stdout=PIPE)
     cmdoutput = process.communicate()
-    exit_code = process.wait()
-    #random_jid = random.choice(range(1000000))
+    #exit_code = process.wait()
     return cmdoutput[0]
-    
+
 def retrieve_job(jid):
+    ''' retrieves a salt job given a jid. '''
     res = run_cmd('salt-run jobs.lookup_jid {} --out=json'.format(jid))
     if not res:
         return False
-    else:
-        return res
+    return res
 
-def foo_func(bar=0):
-    return bar * 2
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
+def get_tasks():  # pylint: disable=missing-docstring
     return jsonify({'tasks': tasks})
 
 @app.route('/')
-def hello():
+def hello():  # pylint: disable=missing-docstring
     return 'hello world!'
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
+def get_task(task_id):  # pylint: disable=missing-docstring
+    tasks_filtered = [task for task in tasks if task['id'] == task_id]
+    if not tasks_filtered:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'task': tasks_filtered[0]})
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
+def create_task():  # pylint: disable=missing-docstring
     if not request.json or not 'title' in request.json:
         abort(400)
     task = {
@@ -77,7 +80,7 @@ def create_task():
 
 
 @app.route('/todo/api/v1.0/salt_jobs/<int:jid>', methods=['GET'])
-def get_job(jid):
+def get_job(jid):  # pylint: disable=missing-docstring
     # example curl:
     # curl -k http://localhost:5000/todo/api/v1.0/salt_jobs/20191109171940356420
     result = retrieve_job(jid)
@@ -86,10 +89,10 @@ def get_job(jid):
     return jsonify({'job': result})
 
 @app.route('/todo/api/v1.0/salt_jobs', methods=['POST'])
-def create_salt_job():
+def create_salt_job():  # pylint: disable=missing-docstring
     # example curl:
-    # curl -k http://localhost:5000/todo/api/v1.0/salt_jobs 
-      #-X POST -H "Content-Type: application/json" 
+    # curl -k http://localhost:5000/todo/api/v1.0/salt_jobs
+      #-X POST -H "Content-Type: application/json"
       #-d '{"command":"test.ping","target":"master3"}'
     if not request.json or not 'command' in request.json:
         abort(400)
@@ -101,4 +104,3 @@ def create_salt_job():
     jid = run_cmd('salt --async {} {} --out=json'.format(job['target'], job['command']))
     job['jid'] = jid
     return jsonify({'job': job}), 201
-
--- src/tklr.py
+++ src/tklr.py
@@ -1,17 +1,25 @@
-from __future__ import print_function
+''' tklr module '''
+from __future__ import print_function, absolute_import
 from collections import OrderedDict, namedtuple
 from datetime import datetime, timedelta
-from dateutil import tz
-from shutil import copyfile, copy
 import fileinput
-import six
+import pdb  # pylint: disable=unused-import
 import re
+from shutil import copy
 
-import pdb
+from dateutil import tz
+import six
 
-Section_Tuple_Class = namedtuple('Section_Tuple', 'name, start, end, full_name')
 
-class Tklr():
+Section_Tuple_Class = namedtuple('Section_Tuple', 'name, start, end, full_name')  # pylint: disable=invalid-name
+
+class Tklr(object):  # pylint:disable=too-many-instance-attributes
+    '''
+    main class for tklr project
+    will contain the contents
+    as well as metadata about sections
+    also config data for sections (as of now)
+    '''
     def __init__(self, filename, no_save, debug=False):
         self.now = datetime.now()
         self.no_save = no_save
@@ -63,9 +71,10 @@ class Tklr():
         return False
 
     def read_file(self, filename):
+        ''' takes a filename, and reads it into self.file_contents var '''
         print('Using file: {}'.format(filename))
-        with open(filename) as f:
-            for one_line in f:
+        with open(filename) as file_object:
+            for one_line in file_object:
                 self.file_contents.append(one_line.strip('\n'))
 
     def load_full_dict(self):
@@ -75,7 +84,7 @@ class Tklr():
             OUTPUT: None? but output artifact is full dict
         '''
         # compute major sections metadata
-        pdb.set_trace() if self.debug else None
+        # pdb.set_trace()
         major = self.searchtags['major_sections']
         tag, ignore_string, regex = major['tag'], major['ignore'], major['regex']
         self.sections = self.find_sections(tag, ignore_string, regex)
@@ -87,12 +96,21 @@ class Tklr():
             name = section.name
             minor = self.searchtags[name]
             tag, ignore_string, regex = minor['tag'], minor['ignore'], minor['regex']
-            minor_metadata_list = self.find_sections(tag, ignore_string, regex, start=section.start, end=section.end)
+            minor_metadata_list = self.find_sections(
+                tag,
+                ignore_string,
+                regex,
+                start=section.start,
+                end=section.end
+            )
 
             # also insert major section (header only) as subsection
             # so everything gets included in subsection list - both major and minor headings
             # otherwise, we will not print major headers
-            major_header = Section_Tuple_Class(section.name, section.start, (minor_metadata_list[0].start)-1, section.full_name )
+            major_header = Section_Tuple_Class(
+                section.name,
+                section.start,
+                (minor_metadata_list[0].start)-1, section.full_name)
             self.subsections.append(major_header)
 
             for one_tuple in minor_metadata_list:
@@ -109,14 +127,16 @@ class Tklr():
         INPUT: None, works mostly with the self.subsections list
         OUTPUT: None, populates the self.dict structure
         '''
-        pdb.set_trace() if self.debug else None
+        #pdb.set_trace()
         for one_subsection in self.subsections:
             self.dict[one_subsection.name] = {}
             self.dict[one_subsection.name]['heading'] = one_subsection.full_name
-            self.dict[one_subsection.name]['contents'] = self.file_contents[one_subsection.start:one_subsection.end]
+            self.dict[one_subsection.name]['contents'] = self.file_contents[
+                one_subsection.start : one_subsection.end
+            ]
 
 
-    def find_sections(self, tag, ignore_string, regex, start=0, end=None):
+    def find_sections(self, tag, ignore_string, regex, start=0, end=None):  # pylint:disable=too-many-arguments
         '''
         SUMMARY: parse input - using magic tags, discover subsections and note their names,
             start and end locations. This only stores metadata about subsections into a
@@ -131,12 +151,20 @@ class Tklr():
         temp_list = self.file_contents[start:] if not end else self.file_contents[start:end]
         ret_list = []
         for line_counter, one_line in enumerate(temp_list):
-            if re.match(regex, one_line) and ignore_string not in one_line and not self.match_keyword(one_line):
-                pdb.set_trace() if self.debug else None
+            if (
+                    re.match(regex, one_line)
+                    and ignore_string not in one_line
+                    and not self.match_keyword(one_line)
+            ):
+                # pdb.set_trace()
                 tag_name = one_line.strip(tag).rstrip('\n')
                 tag_name = tag_name.split(':')[0]
-                section = Section_Tuple_Class(name=tag_name, start=line_counter+1+start,
-                    end=0, full_name = one_line)
+                section = Section_Tuple_Class(
+                    name=tag_name,
+                    start=line_counter+1+start,
+                    end=0,
+                    full_name=one_line
+                )
                 ret_list.append(section)
         # easiest way to calculate section end is to look at start of next element
         #pdb.set_trace()# if self.debug else None
@@ -144,9 +172,19 @@ class Tklr():
             try:
                 section_end = ret_list[count+1].start - 1 #one line before start of next section
             except IndexError:
-                ret_list[count] = Section_Tuple_Class(name=one_section.name, start=one_section.start, end=line_counter+1 + start, full_name=one_section.full_name)
+                ret_list[count] = Section_Tuple_Class(
+                    name=one_section.name,
+                    start=one_section.start,
+                    end=line_counter+1 + start,  # pylint: disable=undefined-loop-variable
+                    full_name=one_section.full_name
+                )
             else:
-                ret_list[count] = Section_Tuple_Class(name=one_section.name, start=one_section.start, end=section_end, full_name=one_section.full_name)
+                ret_list[count] = Section_Tuple_Class(
+                    name=one_section.name,
+                    start=one_section.start,
+                    end=section_end,
+                    full_name=one_section.full_name
+                )
         return ret_list
 
 
@@ -155,20 +193,24 @@ class Tklr():
         inside calendar major section, for each day as int, fix the day name as string
         '''
         # iterate over dict, find only the day sections(regex using the definition?), ignore rest
-        outside_calendar = False
         for sname, section_value in six.iteritems(self.dict):
             #print(sname)
             if re.match(r'\d\d', sname): # ex: match 01 but not M12 or travel
                 try:
-                    replaced_date = datetime.strptime('{}/{}/{}'.format(self.now.month, sname, self.now.year), '%m/%d/%Y')
+                    replaced_date = datetime.strptime(
+                        '{}/{}/{}'.format(self.now.month, sname, self.now.year),
+                        '%m/%d/%Y'
+                    )
                     rep2 = replaced_date.strftime('%a')
                 except ValueError:
                     rep2 = 'N/A'
-                new_name = '{0}{1}: ({2})'.format(self.searchtags['calendar']['tag'],
-                    sname, rep2)
+                new_name = '{0}{1}: ({2})'.format(
+                    self.searchtags['calendar']['tag'], sname, rep2
+                )
                 section_value['heading'] = new_name
 
     def print_today(self):
+        ''' print_today '''
         from_zone = tz.gettz('UTC')
         to_zone = tz.gettz('America/New_York')
         today = datetime.now()
@@ -178,6 +220,9 @@ class Tklr():
 
 
     def move_today(self, day_count):
+        ''' goes back day_count days and
+        moves their section contents to todays section
+        '''
         #day = figure_day():
         # date shananigans
         from_zone = tz.gettz('UTC')
@@ -191,7 +236,7 @@ class Tklr():
         for day_to_go_back_to in range(day_count, 0, -1):
             day_from = today - timedelta(hours=24*day_to_go_back_to)
             day_to = day_from + timedelta(hours=24)
-            # Tell the datetime object that it's in UTC time zone since 
+            # Tell the datetime object that it's in UTC time zone since
             # datetime objects are 'naive' by default
             day_to = day_to.replace(tzinfo=from_zone)
             day_from = day_from.replace(tzinfo=from_zone)
@@ -200,7 +245,12 @@ class Tklr():
             day_to_local = day_to.astimezone(to_zone)
             day_from_local = day_from.astimezone(to_zone)
 
-            print('Moving from {} to {}.'.format(day_from_local.strftime('%d'), day_to_local.strftime('%d')))
+            print(
+                'Moving from {} to {}.'.format(
+                    day_from_local.strftime('%d'),
+                    day_to_local.strftime('%d')
+                )
+            )
             self.move_section(str(day_from_local.strftime('%d')), str(day_to_local.strftime('%d')))
         return 0
 
@@ -214,9 +264,10 @@ class Tklr():
         self.dict[section_from]['contents'] = ''
 
     def get_section(self, section_name, debug=False):
+        ''' retrieves one section and returns it to caller '''
         if debug:
             minors = ''
-            for k, v in six.iteritems(self.dict):
+            for k, _ in six.iteritems(self.dict):
                 minors = minors + k + ','
             print('Minor sections: ({})'.format(minors))
         ret = ''
@@ -226,28 +277,20 @@ class Tklr():
             ret += line + '\n'
         return ret
 
-    def print_section(self, section_name):
-        return 'dont use, use get_section instead'
-        '''
-        ret = ''
-        for line in self.dict[section_name]['contents']:
-            ret += line + '\n'
-        return ret
-        '''
-
-
     @staticmethod
     def generate_unused_bak_file_name(filename):
+        ''' generate_unused_bak_file_name '''
         new_filename = '{}.bak'.format(filename)
         return new_filename
 
     @staticmethod
     def copy_file(src, dst):
+        ''' copy_file '''
         #pdb.set_trace()
-        print('Copying file {} to {}'.format(src,dst))
+        print('Copying file {} to {}'.format(src, dst))
         try:
             copy(src, dst)
-        except:
+        except BaseException:
             print('ERROR copying file {} to {}'.format(src, dst))
             return False
         return True
@@ -256,20 +299,31 @@ class Tklr():
         ''' finds line count of the self.dict structure '''
         count = 0
         #pdb.set_trace()
-        for k, v in six.iteritems(self.dict):
+        for k, _ in six.iteritems(self.dict):
             #print(self.get_section(k))
             count += len(self.get_section(k).split('\n')) - 1
         return count
 
     def save_file(self):
+        '''
+        prints to screen,
+        does safety checks like comparing line counts
+        backs up a copy,
+        then modifies the current file
+        '''
+
         if self.no_save:
             print('no save requested, will print to screen only')
             print(self.__str__())
-            return
+            return True
 
         # check line counts and back out if not equal
         new_len = self.find_len()
-        print('orig line count: {}, new file line count: {}'.format(len(self.file_contents), new_len))
+        print(
+            'orig line count: {}, new file line count: {}'.format(
+                len(self.file_contents), new_len
+            )
+        )
         if abs(len(self.file_contents) - new_len) > 1:
             print('old and new files seem different by line counts, not proceeding')
             return False
@@ -277,9 +331,9 @@ class Tklr():
         bak_filename = self.generate_unused_bak_file_name(self.filename)
         print('saving file: {}'.format(self.filename))
         if self.copy_file(self.filename, bak_filename):
-            with open(self.filename, 'w') as f:
-                for k, v in six.iteritems(self.dict):
-                    f.write(self.get_section(k))
+            with open(self.filename, 'w') as file_object:
+                for k, _ in six.iteritems(self.dict):
+                    file_object.write(self.get_section(k))
                 return True
         else:
             print('backup copy not made, so not saving anything')
@@ -287,12 +341,13 @@ class Tklr():
 
     def __str__(self):
         print_string = ''
-        for k, v in six.iteritems(self.dict):
+        for k, _ in six.iteritems(self.dict):
             print_string += (self.get_section(k))
         return print_string
 
     @staticmethod
     def generate_html_filename(filename):
+        ''' generate name '''
         #pdb.set_trace()
         new_filename = '{}.html'.format(filename)
         return new_filename
@@ -300,22 +355,24 @@ class Tklr():
 
     @staticmethod
     def count_spaces(string):
+        ''' count leading spaces '''
         #pdb.set_trace()
-        for counter,char in enumerate(string):
+        for counter, char in enumerate(string):
             if char != " ":
                 return counter
         return 0
 
     def make_html(self):
+        ''' creates html file from the main dict '''
         html_filename = self.generate_html_filename(self.filename)
         print('saving file: {}'.format(html_filename))
         #pdb.set_trace()
-        with open(html_filename, 'w') as f:
+        with open(html_filename, 'w') as file_object:
             for oneline in self.file_contents:
                 spaces = self.count_spaces(oneline)*"&nbsp"
-                f.write("{}{}{}".format(spaces, oneline.lstrip() ,' <br>\n'))
-                #for k, v in six.iteritems(self.dict):
-                #    f.write(self.get_section(k))
+                file_object.write("{}{}{}".format(spaces, oneline.lstrip(), ' <br>\n'))
+                #for k, _ in six.iteritems(self.dict):
+                #    file_object.write(self.get_section(k))
             return True
 
     def add_headings(self):
@@ -345,8 +402,8 @@ class Tklr():
                     pass
             else:
                 print(line)
-        with open(html_filename, 'a') as f:
-            f.write(html_footer)
+        with open(html_filename, 'a') as file_object:
+            file_object.write(html_footer)
 
     def add_html_toc(self):
         ''' adds Table of Contents '''
@@ -356,5 +413,3 @@ class Tklr():
             ret_string += '  <li><a href="#{0}">{0}</a></li>\n'.format(section_name)
         ret_string += '</ol>'
         return ret_string
-
-
